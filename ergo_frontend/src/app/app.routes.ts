a02ea2ff1e99f64daabd1b9d7af6a0659cc7156e
Michael Rockson <michaelrockszn@gmail.com>
2026-03-13 11:52:45 +0000

    feat: setup application authentication routes

--- ergo_frontend/src/app/app.routes.ts
+++ ergo_frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'auth/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.routes').then(m => m.AUTH_ROUTES),
+  },
+  {
+    path: '**',
+    redirectTo: 'auth/login',
+    pathMatch: 'full',
+  }
+];
