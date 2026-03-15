f290e4a9e3494d3d369ae78d91ed72637383bb76
Michael Rockson <michaelrockszn@gmail.com>
2026-03-15 10:45:43 +0000

    feat: use native angular img optimization

--- ergo_frontend/src/app/features/auth/layout/auth-layout.ts
+++ ergo_frontend/src/app/features/auth/layout/auth-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterOutlet } from "@angular/router";
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
 	selector: "app-layout",
-	imports: [FormsModule, ReactiveFormsModule, RouterOutlet],
+	imports: [FormsModule, ReactiveFormsModule, RouterOutlet, NgOptimizedImage],
 	templateUrl: "./auth-layout.html",
 	styleUrl: "./auth-layout.scss",
 	standalone: true,
