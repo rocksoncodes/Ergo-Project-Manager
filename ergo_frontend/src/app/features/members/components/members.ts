2cabe57eef0206e7cdff70a4328bad927041740f
rocksoncodes <michaelrockszn@gmail.com>
2026-04-04 12:31:23 +0000

    feat(Pages): added remainig system pages
     - setup new system page routes
     - created scoped system page services

---
+++ ergo_frontend/src/app/features/members/components/members.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "app-members",
+	imports: [],
+	templateUrl: "./members.html",
+	styleUrl: "./members.scss",
+})
+export class Members {}
